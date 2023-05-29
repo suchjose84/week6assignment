23f4e5b766fba366f8849b3779c8d8c95c3d99cd
suchjose84 <suchjose@live.com>
2023-05-30 01:17:50 +0800

    fix fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,16 +5,6 @@ const User = db.user;
 const passwordUtil = require('../util/validation_schema');
 
 //get all user data
-
-// exports.getAllUsers = async (req, res, next) => {
-//   try {
-//     const users = await mongodb.getDb().db('eggsandmore').collection('users').find().toArray();
-//     res.status(200).json(users);
-//   } catch (error) {
-//     next(error);
-//   }
-
-// };
 module.exports.getAllUsers = (req, res, next) => {
   try {
     User.find({})
@@ -31,25 +21,10 @@ module.exports.getAllUsers = (req, res, next) => {
   }
 };
 
-//get 1 user
-// exports.getUser = async (req, res, next) => {
-//   const userName = req.params.userName;
-//   try {
-//     const user = await mongodb.getDb().db('eggsandmore').collection('users').findOne({userName: userName});
-//     if (!user) {
-//       return res.status(404).json({
-//         message: 'Contact not found'
-//       });
-//     }
-//     res.status(200).json(user);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
 module.exports.getUser = (req, res, next) => {
   try {
-    const username = req.params.username;
-    User.find({ username: username })
+    const userName = req.params.userName;
+    User.find({ username: userName })
       .then((data) => {
         res.status(200).send(data);
       })
@@ -63,60 +38,6 @@ module.exports.getUser = (req, res, next) => {
   }
 };
 
-
-//create 1 user
-// exports.addUser = async (req, res, next) => {
-//   try {
-//     const user = {
-//       userName: req.body.userName,
-//       firstName: req.body.firstName,
-//       lastName: req.body.lastName,
-//       email: req.body.email,
-//       password: req.body.password,
-//       birthDate: req.body.birthDate,
-//       phone: req.body.phone,
-//       country: req.body.country,
-//       profileImg: req.body.profileImg
-
-//     };
-//     const result = await mongodb.getDb().db('eggsandmore').collection('users').insertOne(user);
-//     res.status(201).json({
-//       id: result.insertedId,
-//       userName: user.userName
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// module.exports.addUser = (req, res) => {
-//   try {
-//     if (!req.body.username || !req.body.password || !req.body.email) {
-//       res.status(400).send({ message: 'Content can not be empty!' });
-//       return;
-//     }
-//     const password = req.body.password;
-//     const passwordCheck = passwordUtil.passwordPass(password);
-//     if (passwordCheck.error) {
-//       res.status(400).send({ message: passwordCheck.error });
-//       return;
-//     }
-//     const user = new User(req.body);
-//     user
-//       .save()
-//       .then((data) => {
-//         console.log(data);
-//         res.status(201).send(data);
-//       })
-//       .catch((err) => {
-//         res.status(500).send({
-//           message: err.message || 'Some error occurred while creating the user.'
-//         });
-//       });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// };
 module.exports.addUser = async (req, res) => {
   try {
     const { error: emailError } = emailSchema.validate(req.body.email);
@@ -131,17 +52,17 @@ module.exports.addUser = async (req, res) => {
       return;
     }
 
-    if (!req.body.username || !req.body.password || !req.body.email) {
-      res.status(400).send({ message: 'Content can not be empty!' });
+    if (!req.body.userName || !req.body.password || !req.body.email) {
+      res.status(400).send({ message: 'Username, password, and email cannot be empty!' });
       return;
     }
 
-    const username = req.body.username;
+    const userName = req.body.userName;
     const email = req.body.email;
 
     const existingUser = await User.findOne({
       $or: [
-        { username: username },
+        { userName: userName },
         { email: email }
       ]
     });
@@ -151,52 +72,29 @@ module.exports.addUser = async (req, res) => {
       return;
     }
 
-    const password = req.body.password;
-    const passwordCheck = passwordUtil.passwordPass(password);
-    if (passwordCheck.error) {
-      res.status(400).send({ message: passwordCheck.error });
-      return;
-    }
+    const newUser = new User({
+      userName: req.body.userName,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      birthDate: req.body.birthDate,
+      phone: req.body.phone,
+      country: req.body.country,
+      profileImg: req.body.profileImg
+    });
 
-    const user = new User(req.body);
-    const savedUser = await user.save();
+    const savedUser = await newUser.save();
     res.status(201).send(savedUser);
   } catch (err) {
     res.status(500).json(err);
   }
 };
 
-
-//edit 1 user
-// exports.editUser = async (req, res, next) => {
-//   try {
-//     const userName = req.params.userName;
-//     const updatedUser = {
-//       userName: req.body.userName,
-//       firstName: req.body.firstName,
-//       lastName: req.body.lastName,
-//       email: req.body.email,
-//       password: req.body.password,
-//       birthDate: req.body.birthDate,
-//       phone: req.body.phone,
-//       country: req.body.country,
-//       profileImg: req.body.profileImg
-//     };
-
-//     const result = await mongodb
-//       .getDb().db('eggsandmore').collection('users').updateOne({ userName }, { $set: updatedUser });
-
-//     res.status(200).json({
-//       message: 'User updated successfully'
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 module.exports.editUser = async (req, res) => {
   try {
-    const username = req.params.username;
-    if (!username) {
+    const userName = req.params.userName;
+    if (!userName) {
       res.status(400).send({ message: 'Invalid Username Supplied' });
       return;
     }
@@ -206,7 +104,7 @@ module.exports.editUser = async (req, res) => {
       res.status(400).send({ message: passwordCheck.error });
       return;
     }
-    User.findOne({ username: username }, function (err, user) {
+    User.findOne({ username: userName }, function (err, user) {
       user.userName = req.body.userName,
       user.firstName = req.body.firstName,
       user.lastName = req.body.lastName,
@@ -229,33 +127,14 @@ module.exports.editUser = async (req, res) => {
   }
 };
 
-// exports.deleteUser = async (req, res, next) => {
-//   const userName = req.params.userName;
-//   try {
-//     const result = await mongodb.getDb().db('eggsandmore').collection('users').deleteOne({ userName });
-
-//     if (result.deletedCount === 0) {
-//       return res.status(404).json({
-//         message: 'User not found'
-//       });
-//     }
-
-//     res.status(200).json({
-//       message: 'User deleted successfully'
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 module.exports.deleteUser = async (req, res) => {
   try {
-    const username = req.params.username;
-    if (!username) {
+    const userName = req.params.userName;
+    if (!userName) {
       res.status(400).send({ message: 'Invalid Username Supplied' });
       return;
     }
-    User.deleteOne({ username: username }, function (err, result) {
+    User.deleteOne({ username: userName }, function (err, result) {
       if (err) {
         res.status(500).json(err || 'Some error occurred while deleting the contact.');
       } else {
